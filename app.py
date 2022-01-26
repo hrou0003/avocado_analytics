2ccf4a96f4ddcccc3c9ccc86fcae71170eada936
hrou0003 <54772688+hrou0003@users.noreply.github.com>
2022-01-26 14:18:20 +1100

    changed to tennis serve statistics

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import numpy as np
+import plotly.express as px
 from dash.dependencies import Input, Output
 
-data = pd.read_csv('avocado.csv')
-data["Date"] = pd.to_datetime(data['Date'], format="%Y-%m-%d")
-data.sort_values("Date", inplace=True)
+data = pd.read_csv('data/serve_statistics.csv')
 
 external_stylesheets = [
 
@@ -22,17 +21,16 @@ external_stylesheets = [
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
-app.title = "Avocado Analytics: Understand your Avocados!"
+app.title = "Tennis Serve Analytics: A breakdown of serve statistics at the 2021 Australian Open"
 
 app.layout = html.Div(
     children=[
             html.Div(
                 children=[
-                    html.P(children="🥑", className="header-emoji"),
-                    html.H1(children="Avocado Analytics", className="header-title"),
-                    html.P(children="Analyse the behavior of avocado prices"
-                    " and the number of avocados sold in the US"
-                    " between 2015 and 2018",
+                    html.P(children="🎾", className="header-emoji"),
+                    html.H1(children="Tennis Serve Analytics", className="header-title"),
+                    html.P(children="Analyse the serving performances"
+                    " of all male players at the 2021 Australian Open",
                     className="header-description",
                     ),
                 ],
@@ -40,30 +38,15 @@ app.layout = html.Div(
             ),
             html.Div(children=[
                 html.Div(
-                children=[
-                    html.Div(children="Region", className="menu-title"),
-                    dcc.Dropdown(
-                        id="region-filter",
-                        options=[
-                            {"label": region, "value": region}
-                            for region in np.sort(data.region.unique())
-                        ],
-                        value="Albany",
-                        clearable=False,
-                        className="dropdown",
-                    ),
-                ],
-            ),
-                html.Div(
                     children=[
-                        html.Div(children="Type", className="menu-title"),
+                        html.Div(children="Category", className="menu-title"),
                         dcc.Dropdown(
-                            id="type-filter",
+                            id="cat-filter",
                             options=[
-                                {"label": avocado_type, "value": avocado_type}
-                                for avocado_type in data.type.unique()
+                                {"label": category, "value": category}
+                                for category in data.columns
                             ],
-                            value="organic",
+                            value="ace",
                             clearable=False,
                             searchable=False,
                             className="dropdown",
@@ -72,19 +55,31 @@ app.layout = html.Div(
                 ),
                 html.Div(
                     children=[
-                        html.Div(
-                            children="Date Range",
-                            className="menu-title",
+                        html.Div(children="Sort", className="menu-title"),
+                        dcc.RadioItems(
+                            id="sort-selector",
+                            options=[
+                                {'label': 'Ascending', 'value': 'True'},
+                                {'label': 'Descending', 'value': 'False'}
+                            ],
+                            value='Ascending',
+                            labelStyle={'display': 'flex'}
                         ),
-                        dcc.DatePickerRange(
-                            id="date-range",
-                            min_date_allowed=data.Date.min().date(),
-                            max_date_allowed=data.Date.max().date(),
-                            start_date=data.Date.min().date(),
-                            end_date=data.Date.max().date(),
-                        ),
-                    ]
+                    ],
                 ),
+                html.Div(
+                    children=[
+                        html.Div(children="# Players", className="menu-title"),
+                        dcc.RangeSlider(
+                            id='disp-number-slider',
+                            min=5,
+                            max=100,
+                            step=5,
+                            value=[20]
+                        ),
+                        html.Div(id="output-disp-number-slider")
+                    ]
+                )
             ],
             className="menu"
         ),
@@ -92,81 +87,84 @@ app.layout = html.Div(
             children=[
                 html.Div(
                     children=dcc.Graph(
-                        id="price-chart", config={"displayModeBar": False},
-                    ),
-                    className="card"
-                ),
-                html.Div(
-                    children=dcc.Graph(
-                        id="volume-chart", config={"displayModeBar": False},
+                        id="cat-chart",
                     ),
                     className="card"
                 ),
             ],
             className="wrapper",
         ),
+        html.Div(
+            children=[
+                html.H1("Individual Statistics"),
+                html.P("Since the Australian Open is a knockout based tournament, the most relevant statistics are those which are normalised."),
+            ],
+            className='desc'
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Div(children="Name", className="menu-title"),
+                        dcc.Dropdown(
+                            id="name-filter",
+                            options=[
+                                {"label": name, "value": name}
+                                for name in np.sort(data.name.unique())
+                            ],
+                            value="Adrian Mannarino",
+                            clearable=False,
+                            className="dropdown",
+                        ),
+                    ],
+                ),
+            ],
+            className="menu body-menu"
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="perc-chart",
+                    ),
+                    className='card center'
+                ),
+            ]
+        )
     ]
 )
 
 @app.callback(
-    [Output("price-chart", "figure"), Output("volume-chart", "figure")],
+    Output("output-disp-number-slider", "children"),
+    [Input("disp-number-slider", "value")]
+)
+def update_output(value):
+    return f"You have selected to display {value[0]} players"
+
+@app.callback(
     [
-        Input("region-filter", "value"),
-        Input("type-filter", "value"),
-        Input("date-range", "start_date"),
-        Input("date-range", "end_date"),
+        Output("cat-chart", "figure"),
+        Output("perc-chart", "figure"),
+    ],
+    [
+        Input("name-filter", "value"),
+        Input("sort-selector", "value"),
+        Input("disp-number-slider", "value"),
+        Input("cat-filter", "value")
     ]
 )
-def update_charts(region, avocado_type, start_date, end_date):
-    mask = (
-        (data.region == region)
-        & (data.type == avocado_type)
-        & (data.Date >= start_date)
-        & (data.Date <= end_date)
-    )
+def update_charts(name, sort, slider, cat):
 
-    filtered_data = data.loc[mask, :]
-    price_chart_figure = {
-        "data": [
-            {
-                "x": filtered_data["Date"],
-                "y": filtered_data["AveragePrice"],
-                "type": "lines",
-                "hovertemplate": "$%{y:.2f}<extra></extra>",
-            },
-        ],
-        "layout": {
-            "title": {
-                "text": "Average Price of Avocados",
-                "x": 0.05,
-                "xanchor": "left",
-            },
-            "xaxis": {"fixedrange": True},
-            "yaxis": {"tickprefix": "$", "fixedrange": True},
-            "colorway": ["#17B897"],
-        },
-    }
+    sort = True if sort=='True' else False
 
-    volume_chart_figure = {
-        "data": [
-            {
-                "x": filtered_data["Date"],
-                "y": filtered_data["Total Volume"],
-                "type": "lines",
-            },
-        ],
-        "layout": {
-            "title": {
-                "text": "Avocados Sold",
-                "x": 0.05,
-                "xanchor": "left",
-            },
-            "axis": {"fixedrange": True},
-            "yaxis": {"fixedrange": True},
-            "colorway": ["#E12D39"],
-        },
-    }
-    return price_chart_figure, volume_chart_figure
+    cat_chart = px.bar(data.sort_values(by=cat, ascending=sort)[:slider[0]], x='name', y=cat)
+
+    perc_data = data[data['name'] == name].T.reset_index()
+    perc_data.columns = ['cats', name]
+
+    perc_chart = px.bar(perc_data[1:-1], x='cats', y=name)
+    
+    return cat_chart, perc_chart
 
 
 if __name__ == "__main__":
